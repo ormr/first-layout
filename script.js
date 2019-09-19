c3dd7a0cb266da8313cab9a28d03af0b7951e4e9
Serafim Gavrilov <seraf.gavrilov@gmail.com>
2019-09-19 22:35:18 +0300

    Changed script a bit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function GCF() {
 	let a = +document.getElementById('num1').value;
 	let b = +document.getElementById('num2').value;
-	console.log(typeof(a))
 	while ( b != 0 && a != 0) {
 		if ( a > b ) {
 			a = a % b;
 		}
 		b = b % a;
 	}
+	let result = a + b;
 	// document.getElementById('result').value = a + b;
-	alert(a + b);
+	alert(result);
 }
