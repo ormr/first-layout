1947d74abf39fa96dddd244b375df1a45c123e09
Serafim Gavrilov <seraf.gavrilov@gmail.com>
2019-09-22 19:56:12 +0300

    Final fix

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function GCF() {
-	let a = +document.getElementById('num1').value;
-	let b = +document.getElementById('num2').value;
-	while ( b != 0 && a != 0) {
-		if ( a > b ) {
+	let a = Number(document.getElementById('num1').value);
+	let b = Number(document.getElementById('num2').value);
+	while (a != 0 && b != 0) {
+		if (a > b) {
 			a = a % b;
+		} else {
+			b = b % a;
 		}
-		b = b % a;
 	}
-	let result = a + b;
-	// document.getElementById('result').value = a + b;
-	alert(result);
+	// output a + b
+	document.write(`
+	<p style="font-size: 44px; font-family: 'Montserrat'; color: #3636b3;">
+		НОД: ${a + b}
+	</p>`);
 }
